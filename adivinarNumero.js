372bf2cd78065644b618d0a9b8c16fdc97f73f3a
tinobosch <tino_boschiazzo@hotmail.com>
2024-01-11 16:10:05 -0300

    Esta es la segunda version que reduce los espacios

--- adivinarNumero.js
+++ adivinarNumero.js
@@ -1,45 +1,30 @@
 let readlineSync = require('readline-sync'); //libreria readline 
                                      // para pedir datos por consola
-const Chance = require('chance');
-const chance = new Chance();
+const Chance = require('chance'); //importo la biblioteca de chance
+const chance = new Chance(); //instancio un objeto de la clase Chance asi puedo usar sus metodos
 
 console.log('Ingrese un numero a adivinar entre 5 y 10: ');
 
 
-let randomNum = chance.integer({min: 1, max: 100});
+let randomNum = chance.integer({min: 1, max: 100}); //seteamos el rango
 
-
-  // Wait for user's response.
+  // Pedimos el numero dentro
   
-  
-
-
   do  {
-
     userNumber = readlineSync.question();
     console.log('Ingresaste ' + userNumber + '! ');
 
- if (userNumber>randomNum) {
+ if (userNumber>randomNum) {  //si es mayor el que ingreso
  console.log('Le erraste! Dijiste ' + userNumber + ' Vuelve a intentar, el numero secreto es menor (una pista: es) ' + randomNum);
- 
  }
 
- else if (userNumber<randomNum) { 
-
-  
+ else if (userNumber<randomNum) { // si es menor el que ingreso
   console.log('Le erraste! Dijiste ' + userNumber + ' Vuelve a intentar, el numero secreto es mayor (una pista: es' + randomNum);
-  
-
-
  }
 
- else if  
-
-  (userNumber == randomNum) {
- 
+ else if  (userNumber == randomNum) { //si lo adivino
   console.log('Acertaste el numero! Dijiste' + userNumber + ' y el numero aleatorio es: ' + randomNum);
-
   }
 }
 
-while (userNumber !== randomNum);
+while (userNumber !== randomNum);  //cuando lo adivine sale del Do y queda en bucle infinito (mal)
